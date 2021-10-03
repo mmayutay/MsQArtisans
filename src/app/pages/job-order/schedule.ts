a224ca0eea51d6f92c28b066327e4c7359dbf6d3
BargasoRC <renanbargaso16@gmail.com>
2021-10-03 19:55:59 +0800

    Initial Push

--- src/app/pages/job-order/schedule.ts
+++ src/app/pages/job-order/schedule.ts
@@ -44,7 +44,6 @@ export class SchedulePage implements OnInit {
   }
 
   updateSchedule() {
-    // Close any open sliding items when the schedule updates
     if (this.scheduleList) {
       this.scheduleList.closeSlidingItems();
     }
